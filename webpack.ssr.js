e48b0c8334d159fe80768b4b9784b4a727fb4bc2
ddai <ddai@tripadvisor.com>
2020-06-16 09:49:01 +1000

    refactored webpack configuration for ssr

--- webpack.ssr.js
+++ webpack.ssr.js
@@ -2,18 +2,46 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
+module.exports = [{
     target: "node",
     node: {
         __dirname: true
     },
     mode: "development",
-    entry: {
-        main: "./src/server/index.tsx",
-        server: "./src/server/server.tsx"
-    },
+    entry: "./src/server/server.tsx",
     output: {
-        filename: "[name].bundle.js",
+        filename: "server.bundle.js",
+        path: path.join(__dirname, "dist/server")
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, // 3. put css into file
+                    "css-loader",   // 2.turn css into commonjs
+                    "sass-loader"   // 1. turn scss into css
+                ]
+            }
+        ]
+    }
+}, {
+    mode: "development",
+    entry: "./src/server/index.tsx",
+    output: {
+        filename: "main.bundle.js",
         path: path.join(__dirname, "dist/server")
     },
     resolve: {
@@ -43,4 +71,4 @@ module.exports = {
             }
         ]
     }
-};
+}];
